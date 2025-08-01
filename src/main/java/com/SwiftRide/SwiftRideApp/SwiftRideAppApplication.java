d3db93a0a1c5f5e32f0c44d3091b4c426fb4679d
Nishant Narula <nishant.narula@gmail.com>
2025-08-01 16:08:47 +0530

    Project Initialization

---
+++ src/main/java/com/SwiftRide/SwiftRideApp/SwiftRideAppApplication.java
@@ -0,0 +1,13 @@
+package com.SwiftRide.SwiftRideApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftRideAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftRideAppApplication.class, args);
+	}
+
+}
