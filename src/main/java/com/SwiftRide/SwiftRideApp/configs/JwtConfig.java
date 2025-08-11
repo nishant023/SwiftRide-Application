fc450c17dba6999c34fd1d94c76a65b5efe00b4e
Nishant Narula <nishant.narula@gmail.com>
2025-08-11 18:48:46 +0530

    Project Building

--- src/main/java/com/SwiftRide/SwiftRideApp/configs/JwtConfig.java
+++ src/main/java/com/SwiftRide/SwiftRideApp/configs/JwtConfig.java
@@ -1,4 +1,22 @@
 package com.SwiftRide.SwiftRideApp.configs;
 
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Configuration
 public class JwtConfig {
+
+    @Value("${jwt.secretKey}")
+    private String jwtSecretKey;
+
+    @Bean
+    public SecretKey secretKey() {
+
+        return Keys.hmacShaKeyFor(jwtSecretKey.getBytes(StandardCharsets.UTF_8));
+    }
 }
--- src/main/java/com/SwiftRide/SwiftRideApp/configs/OpenApiConfig.java
+++ src/main/java/com/SwiftRide/SwiftRideApp/configs/OpenApiConfig.java
@@ -1,4 +1,46 @@
 package com.SwiftRide.SwiftRideApp.configs;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class OpenApiConfig {
+
+    public static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+
+    @Bean
+    public OpenAPI swiftDriveOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Swift Drive API")
+                        .version("v1.0")
+                        .description("API documentation for Swift Drive application")
+                        .contact(new io.swagger.v3.oas.models.info.Contact()
+                                .name("Nishant")
+                                .email("nishant.narula023@gmail.com"))
+                ).addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+
+    // (Optional) If you want to group APIs (like user/contact)
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("Swift-Drive")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
 }
